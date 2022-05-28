047950e79f845c470a4512d348323c86a9bf371c
Taufik-H <taufikrpl1@gmail.com>
2022-05-29 06:28:38 +0700

    protect the answer

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
+window.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    var contextElement = document.getElementById("context-menu");
+    contextElement.style.top = event.offsetY + "px";
+    contextElement.style.left = event.offsetX + "px";
+    contextElement.classList.add("active");
+});
+window.addEventListener("click", function () {
+    var contextElement = document
+        .getElementById("context-menu")
+        .classList.remove("active");
+});
 let soal = [
-  {
-    s: "Ditutup jadi tongkat, dibuka jadi tenda. Apakah itu?",
-    answer: "payung",
-  },
-  {
-    s: "Apa yang ada di ujung langit?",
-    answer: "t",
-  },
-  {
-    s: "Kebo apa yg bikin kita lelah? ",
-    answer: "kebogor jalan kaki",
-  },
-  {
-    s: "Sayur apa yang pintar nyanyi?",
-    answer: "kolplay",
-  },
-  {
-    s: "Apakah huruf keempat dalam abjad",
-    answer: "a",
-  },
-  {
-    s: "kasur rusak dibalik jadi?",
-    answer: "kasur rusak",
-  },
+    {
+        s: "Ditutup jadi tongkat, dibuka jadi tenda. Apakah itu?",
+        answer: "payung",
+    },
+    {
+        s: "Apa yang ada di ujung langit?",
+        answer: "t",
+    },
+    {
+        s: "Kebo apa yg bikin kita lelah? ",
+        answer: "kebogor jalan kaki",
+    },
+    {
+        s: "Sayur apa yang pintar nyanyi?",
+        answer: "kolplay",
+    },
+    {
+        s: "Apakah huruf keempat dalam abjad",
+        answer: "a",
+    },
+    {
+        s: "kasur rusak dibalik jadi?",
+        answer: "kasur rusak",
+    },
 ];
 
 // quis
 const nomorKuis = document.querySelector(".nomor-kuis");
 const textKuis = document.querySelector(".text-kuis");
+const salah = document.getElementById("salah");
 const sudah = document.querySelector(".sudah");
 const buletIndicator1 = document.querySelector(".buletIndicator");
 const start = document.querySelector(".start");
@@ -44,115 +57,122 @@ let jwbbenar = 0;
 let attempt = 0;
 
 function setKuisTersedia() {
-  const totalKuis = soal.length;
-  for (let i = 0; i < totalKuis; i++) {
-    kuisTersedia.push(soal[i]);
-  }
+    const totalKuis = soal.length;
+    for (let i = 0; i < totalKuis; i++) {
+        kuisTersedia.push(soal[i]);
+    }
 }
 function getNewQuestion() {
-  // nomor jumlah kuis
-  nomorKuis.innerHTML = "JOKE BAPAK-BAPAK" + " " + (questionCounter + 1) + "/" + limitKuis;
-  // soalnya
-  // random soal
-  const indexSoal = kuisTersedia[Math.floor(Math.random() * kuisTersedia.length)];
-  kuisSekarang = indexSoal;
-  textKuis.innerHTML = kuisSekarang.s;
+    // nomor jumlah kuis
+    nomorKuis.innerHTML =
+        "JOKE BAPAK-BAPAK" + " " + (questionCounter + 1) + "/" + limitKuis;
+    // soalnya
+    // random soal
 
-  const indexSoal1 = kuisTersedia.indexOf(indexSoal);
-  // hapus lagi indexnya dari array
-  kuisTersedia.splice(indexSoal1, 1);
-  // console.log(indexSoal)
-  // console.log(kuisTersedia)
+    const indexSoal =
+        kuisTersedia[Math.floor(Math.random() * kuisTersedia.length)];
+    kuisSekarang = indexSoal;
+    textKuis.innerHTML = kuisSekarang.s;
 
-  questionCounter++;
+    const indexSoal1 = kuisTersedia.indexOf(indexSoal);
+    // hapus lagi indexnya dari array
+    kuisTersedia.splice(indexSoal1, 1);
+    // console.log(indexSoal)
+    // console.log(kuisTersedia)
+
+    questionCounter++;
 }
 
 function add() {
-  // ambil nilai dari text
-  let jwb = document.getElementById("jwb");
+    // ambil nilai dari text
+    let jwb = document.getElementById("jwb");
 
-  if (jwb.value === kuisSekarang.answer) {
-    updateJwbIndicator("benar");
-    jwbbenar++;
-    console.log("benar : " + jwbbenar);
-  } else {
-    updateJwbIndicator("salah");
-  }
-  jwb.value = "";
-  attempt++;
+    if (jwb.value === kuisSekarang.answer) {
+        updateJwbIndicator("benar");
+        jwbbenar++;
+        console.log("benar : " + jwbbenar);
+    } else {
+        updateJwbIndicator("salah");
+        console.log("salah");
+        salah.innerHTML = "Harusnya : " + kuisSekarang.answer;
+    }
+    jwb.value = "";
+    attempt++;
 
-  sudahjawab();
+    sudahjawab();
 }
+
 // disable button kirim jika sudah menjawab soal gais
 function sudahjawab() {
-  const sudahjawab1 = document.getElementById("sudah");
-  sudahjawab1.classList.add("disable");
+    const sudahjawab1 = document.getElementById("sudah");
+    sudahjawab1.classList.add("disable");
 }
 function buletIndikator() {
-  buletIndicator1.innerHTML = "";
-  const totalSoal = limitKuis;
-  for (let i = 0; i < totalSoal; i++) {
-    const indicator = document.createElement("div");
-    buletIndicator1.appendChild(indicator);
-  }
+    buletIndicator1.innerHTML = "";
+    const totalSoal = limitKuis;
+    for (let i = 0; i < totalSoal; i++) {
+        const indicator = document.createElement("div");
+        buletIndicator1.appendChild(indicator);
+    }
 }
 function updateJwbIndicator(markType) {
-  buletIndicator1.children[questionCounter - 1].classList.add(markType);
+    buletIndicator1.children[questionCounter - 1].classList.add(markType);
 }
 
 function next() {
-  if (questionCounter === limitKuis) {
-    kuisSelesai();
-  } else {
-    getNewQuestion();
-    const sudahjawab1 = document.getElementById("sudah");
-    sudahjawab1.classList.remove("disable");
-  }
-  jwb.value = "";
+    if (questionCounter === limitKuis) {
+        kuisSelesai();
+    } else {
+        getNewQuestion();
+        const sudahjawab1 = document.getElementById("sudah");
+        sudahjawab1.classList.remove("disable");
+    }
+    jwb.value = "";
+    salah.innerHTML = "";
 }
 
 function kuisSelesai() {
-  now.classList.add("hidden");
-  result.classList.remove("hidden");
-  kuisResult();
+    now.classList.add("hidden");
+    result.classList.remove("hidden");
+    kuisResult();
 }
 function kuisResult() {
-  result.querySelector(".total-soal").innerHTML = limitKuis;
-  result.querySelector(".attempt").innerHTML = attempt;
-  result.querySelector(".benar").innerHTML = jwbbenar;
-  result.querySelector(".salah").innerHTML = attempt - jwbbenar;
-  const persentasi = (jwbbenar / limitKuis) * 100;
-  result.querySelector(".persentase").innerHTML = persentasi.toFixed(2) + "%";
-  result.querySelector(".score").innerHTML = jwbbenar + "/" + limitKuis;
+    result.querySelector(".total-soal").innerHTML = limitKuis;
+    result.querySelector(".attempt").innerHTML = attempt;
+    result.querySelector(".benar").innerHTML = jwbbenar;
+    result.querySelector(".salah").innerHTML = attempt - jwbbenar;
+    const persentasi = (jwbbenar / limitKuis) * 100;
+    result.querySelector(".persentase").innerHTML = persentasi.toFixed(2) + "%";
+    result.querySelector(".score").innerHTML = jwbbenar + "/" + limitKuis;
 }
 function resetKuis() {
-  questionCounter = 0;
-  jwbbenar = 0;
-  attempt = 0;
-  kuisTersedia = [];
+    questionCounter = 0;
+    jwbbenar = 0;
+    attempt = 0;
+    kuisTersedia = [];
 }
 function TryAgain() {
-  result.classList.add("hidden");
-  start.classList.remove("hidden");
-  resetKuis();
-  startkuis();
+    result.classList.add("hidden");
+    start.classList.remove("hidden");
+    resetKuis();
+    startkuis();
 }
 function Home() {
-  result.classList.add("hidden");
-  start.classList.remove("hidden");
-  resetKuis();
+    result.classList.add("hidden");
+    start.classList.remove("hidden");
+    resetKuis();
 }
 function startkuis() {
-  start.classList.add("hidden");
-  now.classList.remove("hidden");
+    start.classList.add("hidden");
+    now.classList.remove("hidden");
 
-  const sudahjawab1 = document.getElementById("sudah");
-  sudahjawab1.classList.remove("disable");
+    const sudahjawab1 = document.getElementById("sudah");
+    sudahjawab1.classList.remove("disable");
 
-  setKuisTersedia();
-  getNewQuestion();
-  buletIndikator();
+    setKuisTersedia();
+    getNewQuestion();
+    buletIndikator();
 }
 window.onload = function () {
-  start.querySelector(".kerjakan").innerHTML = limitKuis;
+    start.querySelector(".kerjakan").innerHTML = limitKuis;
 };
